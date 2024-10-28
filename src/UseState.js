41f6e56ddb2ac81825a610d69e3af877e50ffb6d
Kellbis <holasoykellbis@gmail.com>
2024-10-28 08:44:02 -0400

    Imperative states with useState

--- src/UseState.js
+++ src/UseState.js
@@ -8,6 +8,8 @@ const UseState = ({ name }) => {
 		voidError: false,
 		error: false,
 		loading: false,
+		confirmed: false,
+		deleted: false,
 	});
 	// With compose states with useState hook, remember to use the spread operator to keep the previous state.
 
@@ -24,34 +26,66 @@ const UseState = ({ name }) => {
 					setState({ ...state, loading: false, voidError: false, error: true });
 					return;
 				}
-				setState({ ...state, loading: false, voidError: false, error: false });
+				setState({ ...state, loading: false, voidError: false, error: false, confirmed: true });
 			}, 2000);
 		}
 	}, [state.loading]);
-	return (
-		<div>
-			<h2>Delete {name}</h2>
-			<p>Please, type the secure code to delete this component.</p>
+	if (!state.confirmed && !state.deleted) {
+		return (
+			<div>
+				<h2>Delete {name}</h2>
+				<p>Please, type the secure code to delete this component.</p>
 
-			{state.voidError && <p className="error">Error: Cannot be empty</p>}
-			{state.error && <p className="error">Error: Wrong code</p>}
-			{state.loading && <p className="loading">Loading...</p>}
+				{state.voidError && <p className="error">Error: Cannot be empty</p>}
+				{state.error && <p className="error">Error: Wrong code</p>}
+				{state.loading && <p className="loading">Loading...</p>}
 
-			<input
-				placeholder="Security code"
-				value={state.value}
-				onChange={(e) => {
-					setState({ ...state, value: e.target.value });
-				}}
-			/>
-			<button
-				onClick={() => {
-					setState({ ...state, loading: true, voidError: false, error: false }); // Set all the errors in false mode for avoid that errors appears in a new validation
-				}}>
-				Check
-			</button>
-		</div>
-	);
+				<input
+					placeholder="Security code"
+					value={state.value}
+					onChange={(e) => {
+						setState({ ...state, value: e.target.value });
+					}}
+				/>
+				<button
+					onClick={() => {
+						setState({ ...state, loading: true, voidError: false, error: false }); // Set all the errors in false mode for avoid that errors appears in a new validation
+					}}>
+					Check
+				</button>
+			</div>
+		);
+	} else if (!!state.confirmed && !state.deleted) {
+		return (
+			<React.Fragment>
+				<p>Are you sure you want to delete this component?</p>
+				<button
+					onClick={() => {
+						setState({ ...state, deleted: true });
+					}}>
+					Yes, delete
+				</button>
+				<button
+					onClick={() => {
+						setState({ ...state, confirmed: false });
+					}}>
+					No, go back
+				</button>
+			</React.Fragment>
+		);
+	} else {
+		return (
+			<React.Fragment>
+				<p>The component has been deleted</p>
+				<button
+					onClick={() => {
+						setState({ ...state, confirmed: false, deleted: false, value: '' });
+					}}>
+					Reset and go back
+				</button>
+			</React.Fragment>
+		);
+	}
 };
 
 export { UseState };
